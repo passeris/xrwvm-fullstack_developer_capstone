496350cb4959153436d8032bbfa7dd5a62fdeb88
Carlos Ramos <ibm_fs_student.9h7rb@passmail.net>
2025-04-14 03:55:47 -0400

    Adding logout routes and registration pages

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -62,10 +62,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),
+            os.path.join(BASE_DIR, 'frontend/build/static')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -140,9 +139,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend/build/static')
 ]
 
