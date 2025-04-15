b11ea41221b8e1466521150071a1f00fb1e9d2a3
passeris <94304985+passeris@users.noreply.github.com>
2025-04-15 17:18:19 +0200

    Reducing lint warnings in indentations

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {
@@ -26,7 +27,7 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+        car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
 
 
     # Create CarModel instances with the corresponding CarMake instances
@@ -124,4 +125,4 @@ def initiate():
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
