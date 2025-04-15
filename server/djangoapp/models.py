e42b18bdc8ea71c5c4f6247bf3fd38ad1f65e00d
Carlos Ramos <ibm_fs_student.9h7rb@passmail.net>
2025-04-15 06:55:11 -0400

    Fixing issue with reviews not displaying

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -39,9 +39,9 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(default=2025,
         validators=[
-            MaxValueValidator(2023),
+            MaxValueValidator(2025),
             MinValueValidator(2015)
         ])
     # Other fields as needed
