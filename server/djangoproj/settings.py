21ec18ad37362e78a1bdb7ae7a887fb51d094704
passeris <94304985+passeris@users.noreply.github.com>
2025-04-15 17:41:36 +0200

    Correcting lint warnings

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,11 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    'https://cramos123-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+  'localhost',
+  'https://cramos123-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://cramos123-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+  'https://cramos123-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
