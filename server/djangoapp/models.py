0aa9c0315bf31d9e0a4ccb81380a3741ae1c0cb8
passeris <94304985+passeris@users.noreply.github.com>
2025-04-15 17:33:25 +0200

    Correcting lint warnings

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
-    def __str__():
+    def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
@@ -23,7 +25,6 @@ class CarModel(models.Model):
             MaxValueValidator(2025),
             MinValueValidator(2015)
         ])
-    # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
