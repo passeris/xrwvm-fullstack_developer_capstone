c435e762f7fa0e1af180f78bc6d9502737c387f2
passeris <94304985+passeris@users.noreply.github.com>
2025-04-16 14:09:08 +0200

    Correcting lint warnings

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,7 +11,10 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -20,7 +23,8 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2025,
+    year = models.IntegerField(
+        default=2025,
         validators=[
             MaxValueValidator(2025),
             MinValueValidator(2015)
