3ee6d16432deea3c9dd7678f54acbce72a06da90
Carlos Ramos <ibm_fs_student.9h7rb@passmail.net>
2025-04-16 10:36:16 -0400

    Fixing issues in migration script to populate database with make and model

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,6 +3,6 @@
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
-python manage.py migrate --noinput --run-syncdb
+python manage.py migrate  --run-syncdb --noinput
 python manage.py collectstatic --noinput
 exec "$@"
